ed94152663d504d616ea58aa48db5c2fd540598d
HiepTD <hiepqd02@gmail.com>
2023-12-26 23:04:27 +0700

    delete test

--- task-manager/src/Components/Pages/Board/index.js
+++
@@ -1,4 +0,0 @@
-const Board = () => {
-  return <div>Board</div>;
-};
-export default Board;
--- task-manager/src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userReducer from './Slices/userSlices'
-const Store = configureStore({
-    reducer: {
-        user: userReducer
-    }
-})
-
-export default Store
