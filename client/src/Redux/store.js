bec9d9e05041fb1d16237802bc6a3091848deb9d
rdJS <127029284+VanThanhHoang@users.noreply.github.com>
2023-12-31 22:31:45 +0700

    editcard

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from './Slices/userSlices'
 import alertReducer from './Slices/alertSlice'
+import cardReducer from './Slices/cardSlice'
+
 const Store = configureStore({
     reducer: {
         user: userReducer,
         alert: alertReducer,
+        store:cardReducer,
     }
 })
 
